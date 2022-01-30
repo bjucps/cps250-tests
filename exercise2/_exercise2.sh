a131b3b8edbe45be4a62f7c88a9d86d73900e2aa
Stephen Schaub <sschaub@bju.edu>
2022-01-29 22:03:25 -0500

    Update

--- exercise2/_exercise2.sh
+++ exercise2/_exercise2.sh
@@ -2,6 +2,7 @@ require-files --test-message "linkcheck.sh submitted" linkcheck.sh
 
 exit-if-must-pass-tests-failed
 
+# start web server
 sudo python3  -m http.server 80 --directory $TEST_DIR &
 sleep 1
 
@@ -11,6 +12,6 @@ rm *.txt *.html 2>/dev/null
 chmod +x linkcheck.sh
 run-program --test-message "linkcheck runs without error" --timeout 5 --showoutputonpass ./linkcheck.sh http://localhost/tests/ex2sample.html
 
-sudo pkill -f python3
+sudo pkill -f python3 # kill web server
 
 require-pdf report.pdf
