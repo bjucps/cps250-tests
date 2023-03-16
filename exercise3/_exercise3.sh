3f46e380da9cf2b2bd4e64cb98797ba0f5b21b2d
Jordan Jueckstock <jpjuecks@bju.edu>
2023-03-16 10:37:04 -0400

    auto grader test updates

--- exercise3/_exercise3.sh
+++ exercise3/_exercise3.sh
@@ -3,7 +3,8 @@ require-files args.c
 
 # ---- Compile --------
 
-do-compile --always-show-output gcc -g  $TEST_DIR/ex3_args_test.c args.c -oargs -lbsd
+do-compile --always-show-output gcc -g  $TEST_DIR/ex3_args_test.c args.c -oargs_asan -fsanitize=address -lbsd
+do-compile --always-show-output gcc -g  $TEST_DIR/ex3_args_test.c args.c -oargs_valgrind -lbsd
 
 exit-if-must-pass-tests-failed
 
@@ -11,5 +12,6 @@ require-pdf report.pdf
 
 forbidden-string-function-check args.c
 
-run-program --test-message "valgrind executes with no errors" --showoutputonpass valgrind ./args | sed 's/^==[^=]*==/====/'
+run-program --test-message "ASAN executes with no errors" --showoutputonpass ./args_asan | sed 's/^==[^=]*==/====/'
+run-program --test-message "valgrind executes with no errors" --showoutputonpass valgrind ./args_valgrind | sed 's/^==[^=]*==/====/'
 
