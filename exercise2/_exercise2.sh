a8dfcc538efc567c9e7096dd9a7afca6c0769e99
Jordan Jueckstock <jpjuecks@bju.edu>
2023-08-08 16:09:08 -0400

    missing commits

--- exercise2/_exercise2.sh
+++ exercise2/_exercise2.sh
@@ -3,14 +3,14 @@ require-files --test-message "linkcheck.sh submitted" linkcheck.sh
 exit-if-must-pass-tests-failed
 
 # start web server
-sudo python3  -m http.server 80 --directory $TEST_DIR &
+python3  -m http.server 8080 --directory $TEST_DIR &
 sleep 1
 
 # Remove any .txt or .html files, if submitted...
 rm *.txt *.html 2>/dev/null
 
-run-program --test-message "linkcheck runs without error" --timeout 5 --showoutputonpass bash linkcheck.sh http://localhost/tests/ex2sample.html
+run-program --test-message "linkcheck runs without error" --timeout 5 --showoutputonpass bash linkcheck.sh http://localhost:8080/tests/ex2sample.html
 
-sudo pkill -f python3 # kill web server
+pkill -f python3 # kill web server
 
 require-pdf report.pdf
