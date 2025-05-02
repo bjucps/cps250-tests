cfa660117c8111fc34e23420662f95ef4ec5928d
Jordan Jueckstock <jpjuecks@bju.edu>
2025-05-02 18:29:17 -0400

    clarifying test criteria for ucon.py

--- project2/_project2.sh
+++ project2/_project2.sh
@@ -1,4 +1,16 @@
 require-pdf report.pdf
 require-files ucon.py
+
+if [ -f ucon.py ]; then
+	status=$(test -x ucon.py && echo "PASS" || echo "FAIL")
+	report-result "$status" "Advisory" "./ucon.py marked 'executable'"
+	
+	status=$(head -n1 ucon.py | grep -q "^#!" && echo "PASS" || echo "FAIL")
+	report-result "$status" "Advisory" "./ucon.py has '#!' shebang line"
+fi
+
+echo "Using $(python3 --version)..."
+echo
+
 run-program --test-category "Sanity Check" --test-message "Demo ./ucon.py --help" --showoutputonpass ./ucon.py --help
 
